277b4ea43399d65b3a454045d913f6e6a7841c32
jimsby <jimsby@gmail.com>
2022-01-10 12:49:21 +0300

    0_1 Project created

---
+++ src/test/java/jimsby/restorantvotingservice/RestorantVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package jimsby.restorantvotingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestorantVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
